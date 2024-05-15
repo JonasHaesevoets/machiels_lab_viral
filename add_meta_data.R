e15b9536a98cffae40f63140d194f7af7043af15
JonasHaesevoets <jonas_haesevoets@hotmail.com>
2024-05-15 10:52:29 +0200

    new version of add metadata

--- add_meta_data.R
+++ add_meta_data.R
@@ -2,6 +2,15 @@ easypackages::libraries("Seurat", "tidyverse", "tidyseurat")
 obj.v5 <- read_rds("../../Documents/machiels_lab_viral/intermediate_data/seurat_obj_central.rds")
 DefaultAssay(obj.v5) <- "RNA"
 
+# Filter metadata columns in obj.v5 starting with "RNA_snn"
+filtered_metadata <- obj.v5@meta.data[, !grepl("^RNA_snn", colnames(obj.v5@meta.data))]
+obj.v5@meta.data <- filtered_metadata
+print(colnames(obj.v5@meta.data))
+
+# Filter metadata columns in obj.v5 starting with "harmony" except "harmony_clusters_0.18"
+filtered_metadata <- obj.v5@meta.data[, !grepl("^harmony", colnames(obj.v5@meta.data))]
+obj.v5@meta.data <- filtered_metadata
+print(colnames(obj.v5@meta.data))
 
 obj.v5 |>  as_tibble() |>
   separate(orig.ident, sep = "__", into = c("day", "sample_type"), remove = FALSE) |> 
@@ -11,26 +20,9 @@ obj.v5 |>  as_tibble() |>
          sample_type=as_factor(sample_type),
          condition=as_factor(condition),
          sampletag_Ms4a3=as_factor(sampletag_Ms4a3),
-         harmony_cluster_8dims_rough=as_factor(harmony_cluster_8dims_rough)
+         seurat_clusters=as_factor(seurat_clusters)
   )
 
-## from case_study_DeSeq2_exp1_sex_demultiplex.qmd
-
-y_chromosome_genes<-   c("Ddx3y", "Eif2s3y", "Kdm5d")
-
-obj.v5$sex_classifier_2 <- obj.v5 |> 
-  join_features(features=c(y_chromosome_genes, "Xist"), slot="data", shape="wide") |>
-  #as_tibble() |> 
-  rowwise() |> 
-  mutate(max_male_genes=max(c_across(Ddx3y:Kdm5d))
-  ) |> 
-  mutate(sex_classifier_2=case_when(
-    (Xist==0)&(max_male_genes==0)~"no_expression_of_specific_genes",
-    (Xist>0)&(max_male_genes==0)~"female",
-    (Xist==0)&(max_male_genes>0)~"male",
-    (Xist>0)&(max_male_genes>0)~"detection_of_male_and_female_genes",
-    TRUE~"something is wrong")) |> 
-  pull(sex_classifier_2)
 
 
 
